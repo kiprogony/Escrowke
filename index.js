167c737b7e377b0b54abce41a95eb88525b9bd5e
Daryl Rogony <darylrogony@gmail.com>
2022-04-21 11:21:26 +0300

    create a html file, insert navigation tags with some elements

---
+++ index.js
@@ -0,0 +1,22 @@
+  window.onscroll = () => {
+      var nav = document.querySelector('nav');
+
+      if (window.pageYOffset > 0) {
+        nav.classList.add("sticky")
+      }
+      else {
+        nav.classList.remove("sticky");
+      }
+    }
+    var button = document.getElementById("mpesaButton");
+
+<function(d, s, id) {
+    let js, mpesa = d.getElementByTagName(s)[0];
+    if (d.getElementById(id)) {return;}
+
+    
+}
+
+
+
+
